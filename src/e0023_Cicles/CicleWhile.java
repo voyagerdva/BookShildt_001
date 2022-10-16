ccd263790085df7934417489d945691a6a1133ef
voyagerdva <mariner-9@yandex.ru>
2022-10-16 21:26:01 +0300

    e0023 Circles - for, while, do-while

---
+++ src/e0023_Cicles/CicleWhile.java
@@ -0,0 +1,12 @@
+package e0023_Cicles;
+
+public class CicleWhile {
+    public static void main(String[] args) {
+        int n = 10;
+
+        do {
+            System.out.println("такт - " + n);
+            n--;
+        } while (n > 10);
+    }
+}
